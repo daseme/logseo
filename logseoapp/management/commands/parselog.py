11cf2a80e7607366be29eed92b5f23f3ddeccfc3
kolmstead <kurt@koanalytics.com>
2012-08-15 11:47:13 -0700

    multi-client support working for parselog cusotm management function

--- logseoapp/management/commands/parselog.py
+++ logseoapp/management/commands/parselog.py
@@ -23,13 +23,14 @@ The return dictionary from the parse function;
     get_request_path(): pull the requested path from %r apache field
 
 """
+import os
 from cProfile import Profile
 import pstats
 from optparse import make_option
 from django.core.management.base import BaseCommand #, CommandError
-from logseoapp.models import LogSeRank, Engine, Kw, Page
+from logseoapp.models import LogSeRank, Engine, Kw, Page, Client
 from datetime import datetime
-import glob
+#import glob
 import urlparse
 import re
 import apachelog
@@ -40,9 +41,10 @@ import searchengines
 import chardet
 import logging
 
+ROOT_PATH = os.path.dirname(__file__)
 
 class Command(BaseCommand):
-    #args = '<logfile logfile ...>'
+    #args = '<client_id >'
     #help = 'parses logfile for insert/update db'
     option_list = BaseCommand.option_list + (
     make_option('--profile',
@@ -55,9 +57,32 @@ class Command(BaseCommand):
 
     def _handle(self, *args, **options):
 
-        for filename in glob.glob('/home/kurt/websites/logseo/logseoapp/management/commands/logs/access.20110901'):
 
-            dicts = parse_log(filename)
+        client_list,client_choice = client_choose()
+
+
+        try:
+            log_path_exists,client_id,client_name,log_path = client_search(client_choice,client_list)
+            self.stdout.write(" you have a client id of %d and a client name of %s w log path %s" % (client_id,client_name,log_path))
+            self.ready = raw_input("Ready to Start ? (yes/no) >> ")
+
+            if self.ready == 'yes':
+                self.stdout.write('yes you are ready....starting .... \n')
+
+            else:
+                self.stdout.write('ok, not ready \n')
+                exit()
+
+        except:
+            client_add(client_choice)
+
+
+        file_list = os.listdir(log_path)
+        file_list.sort()
+
+        for filename in file_list:
+
+            dicts = parse_log(log_path + filename)
             parsed_list = []
 
             for d in dicts:
@@ -67,15 +92,22 @@ class Command(BaseCommand):
 
                 try:
                     encoding['encoding'] == 'ascii'
-                    phrase_id      = check_phrase(log_phrase,d['refdate'])
+                    phrase_id      = check_phrase(log_phrase,d['refdate'],client_id)
                     engine_id      = check_engine(d['engine'])
-                    page_id        = check_page(d['page'])
+                    page_id        = check_page(d['page'],client_id)
+                    d['client_id'] = Client.objects.get(id=client_id)
                     d['phrase_id'] = Kw.objects.get(pk=phrase_id)
                     d['engine_id'] = Engine.objects.get(pk=engine_id)
                     d['page_id']   = Page.objects.get(pk=page_id)
-                    parsed_list.append({'ip':d['ip'],'position':d['position'],'phrase_id':d['phrase_id'],
-                                      'page_id':d['page_id'],'engine_id':d['engine_id'],'http':d['http'],
-                                      'refdate':d['refdate'],'reftime':d['reftime']})
+                    parsed_list.append({'ip':d['ip'],
+                                        'position':d['position'],
+                                        'phrase_id':d['phrase_id'],
+                                        'page_id':d['page_id'],
+                                        'engine_id':d['engine_id'],
+                                        'http':d['http'],
+                                        'refdate':d['refdate'],
+                                        'reftime':d['reftime'],
+                                        'client_id':d['client_id']})
 
                 except:
                     logging.basicConfig(filename='example.log',level=logging.INFO)
@@ -83,7 +115,7 @@ class Command(BaseCommand):
                     dicts.remove(d)
 
 
-            print len(parsed_list)
+            print(" length of parsed list is %d" % len(parsed_list))
             my_objects = [LogSeRank(**vals) for vals in parsed_list]
 
             LogSeRank.objects.bulk_create(my_objects)
@@ -104,13 +136,72 @@ class Command(BaseCommand):
         else:
             self._handle(*args, **options)
 
+def client_choose():
+    client_list = Client.objects.values('id','name')
+
+    print('Select client name from the following clients:\n')
+
+    for client in client_list:
+        print(' "%d - name = %s"\n' % (client['id'],client['name']))
+
+    client_choice = raw_input(">> ")
+
+    return client_list,client_choice
+
+def client_search(client, client_list):
+    c_list = [element for element in client_list if element['name'] == client]
+
+    if c_list:
+        return check_log_path(c_list)
+    else:
+        return False
+
+def client_add(client_choice):
+
+    client_check = Client.objects.filter(name = client_choice)
+
+    if client_check:
+        print('We found your client, you probably need to add the path listed above')
+        exit()
+
+    else:
+        print('Client not found, would you like to add this client?')
+
+
+        add_client = raw_input("Add Client ? (yes/no) >> ")
+
+        if add_client == 'yes':
+            client_to_add = raw_input("What is the Client Name ? >> ")
+
+            client_new = Client(name=client_to_add)
+            client_new.save()
+            x = Command()
+            x._handle()
+
+        else:
+            print('will not add client, good bye \n')
+            exit()
+
+def check_log_path(c_list):
+    client_id = c_list[0]['id']
+    client_name = c_list[0]['name']
+    log_path = ROOT_PATH + '/logs/' + client_name + '/'
+
+    if os.path.lexists(log_path):
+        log_path_exists = True
+        return log_path_exists,client_id,client_name,log_path
+
+    else:
+        bad_path = True
+        print('We did not find any log file in %s, pls add files then start over \n\n' % log_path)
+        return bad_path
 
 
 """
  db functions
 """
 
-def check_phrase(log_phrase,refdate):
+def check_phrase(log_phrase,refdate,client_id):
 
     get_phrase = Kw.objects.values('id','phrase','last_seen').filter(phrase = log_phrase)
 
@@ -121,7 +212,7 @@ def check_phrase(log_phrase,refdate):
         return get_phrase[0]['id']
 
     else:
-        p = Kw(phrase = log_phrase, first_seen = refdate, last_seen = refdate)
+        p = Kw(phrase = log_phrase, first_seen = refdate, last_seen = refdate, client_id = client_id)
         p.save()
         return p.id
 
@@ -140,7 +231,7 @@ def check_engine(log_engine):
         return p.id
 
 
-def check_page(log_page):
+def check_page(log_page, client_id):
 
     get_page = Page.objects.values('id','page').filter(page = log_page)
 
@@ -148,10 +239,9 @@ def check_page(log_page):
         return get_page[0]['id']
 
     else:
-        p = Page(page = log_page)
+        p = Page(page = log_page, client_id = client_id)
         p.save()
         return p.id
-
 
 
 """
